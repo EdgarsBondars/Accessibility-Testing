453e4035925c2fbd23743c4936f7c741aa345076
Edgars Bondars <feey1@LAPTOP-CRMF880G>
2024-04-15 21:56:38 +0300

    Started implementing violations.

---
+++ Accessibility Testing/Controllers/WCAG_AController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accessibility_Testing.Controllers
+{
+    public class WCAG_AController : Controller
+    {
+        public IActionResult GuideLine1_1()
+        {
+            return View();
+        }
+    }
+}
