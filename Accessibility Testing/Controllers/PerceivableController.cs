b6d1132c6858ba96d8c95b631ba3748a1f0a4ef7
Edgars Bondars <feey1@LAPTOP-CRMF880G>
2024-04-22 21:29:26 +0300

    Little cleanup + structure + unit test project.

---
+++ Accessibility Testing/Controllers/PerceivableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accessibility_Testing.Controllers
+{
+    public class PerceivableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Accessibility Testing/Controllers/RobustController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accessibility_Testing.Controllers
+{
+    public class RobustController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
